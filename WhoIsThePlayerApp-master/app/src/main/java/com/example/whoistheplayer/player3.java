1c640b10070d27c091afb6256c53eb7341774724
tylerhays987 <126112907+tylerhays987@users.noreply.github.com>
2023-03-20 14:42:16 -0600

    Add files via upload

---
+++ WhoIsThePlayerApp-master/app/src/main/java/com/example/whoistheplayer/player3.java
@@ -0,0 +1,13 @@
+package com.example.whoistheplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class player3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player3);
+    }
+}
